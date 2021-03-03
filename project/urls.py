3b8e14a1105bd8a7d349f02c7f580102e967a7fb
rickyc <r.cato@live.com>
2021-03-03 16:24:09 +0000

    img folder

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from .views import index
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +11,6 @@ urlpatterns = [
     path('api/artists/', include('artists.urls')),
     path('api/labels/', include('labels.urls')),
     path('api/tags/', include('tags.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/rr-logo.ico'))),
     re_path(r'^.*$', index),
 ]
